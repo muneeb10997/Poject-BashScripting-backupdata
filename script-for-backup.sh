c3618312cf2df9676f4a812f08fc5a3dbc7f6c02
Muneeb <muneeburrehman10997@gmail.com>
2024-09-14 20:05:52 +0500

    Updated Script_for_backup.sh file

--- script-for-backup.sh
+++ script-for-backup.sh
@@ -3,28 +3,38 @@
 # Get the folder path from the argument 
 Input_Folder_Path="$1"
 
-# Backup folder path (Update this to a valid Linux path)
-Backup_Folder_Path="/home/muneeb/Desktop/bash_script_project/Backup" 
+# Backup folder path 
+Backup_Folder_Path="$2" 
 
 # Get the name of the folder from the input path
 folder_name=$(basename "$Input_Folder_Path")
 
-
+# get the input folder path except the folder you want to backup
 folder_dir=$(dirname "$Input_Folder_Path")
 
-# Setting the current timestamp with a good format
+# Setting the current timestamp
 date_time=$(date +"%Y-%m-%d_%H-%M-%S") 
 
+# Check the Input folder path is provided or not
+if [ "$#" -ne 2 ]
+then
+    echo "please provide both input and ouput folders paths"
+    exit 1
+fi
 
-# Check if the argument is passed
-if [ "$#" -ne 1 ]; then
-    echo "The path of the folder is not provided."
+
+# Check if the provided path is a valid directory
+if [ ! -d "$Input_Folder_Path" ]
+then 
+    echo "The input path $Input_Folder_Path is not a valid directory."
     exit 1 
 fi
 
+
 # Check if the provided path is a valid directory
-if [ ! -d "$Input_Folder_Path" ]; then 
-    echo "The input path $Input_Folder_Path is not a valid directory."
+if [ ! -d "$Backup_Folder_Path" ]
+then 
+    echo "The backup path $Backup_Folder_Path is not a valid directory."
     exit 1 
 fi
 
@@ -34,13 +44,12 @@ echo "Backup Folder Name : $folder_name"
 zip_file_name="${folder_name}-${date_time}.zip" 
 zip_file_path="$Backup_Folder_Path/$zip_file_name"
 
-
 # Remove the previous zip file if it exists
 base_zip_file_name="${folder_name}*.zip"
-for old_zip in "$Backup_Folder_Path"/$base_zip_file_name;
+for old_zip in "$Backup_Folder_Path"/$base_zip_file_name
 do
     if [ -f "$old_zip" ]
-	then
+    then
         echo "Removing existing zip file: $old_zip"
         rm "$old_zip"
     fi
@@ -50,8 +59,9 @@ done
 echo "Creating Zip of the folder: $folder_name"
 (cd "$folder_dir" && zip -r "$zip_file_path" "$folder_name")
 
-# Print success message
-if [ $? -eq 0 ]; then
+# Print success message for backup done or not
+if [ $? -eq 0 ]
+then
     echo "Backup successfully created at: $zip_file_path"
 else
     echo "Failed to create backup."
